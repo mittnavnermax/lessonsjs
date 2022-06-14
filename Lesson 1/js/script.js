6fa8c8cf035ae2399da4ee0bddf0ece1bb02f500
mittnavnermax <empinvoke@gmail.com>
2022-06-14 22:34:01 +0300

    first commit

---
+++ Lesson 1/js/script.js
@@ -0,0 +1,11 @@
+let userName = prompt("Enter Your name, please");
+let userSurname = prompt("Enter Your surname, please");
+let userAge = prompt("Now enter Your age, please");
+let userHobby = prompt("Name one of your hobbies, please");
+let userCity = prompt("Enter the name of your city, please");
+
+console.log ("Your name is " + userName + " " + userSurname + ", Your age is " + userAge + ", You are keen on " + userHobby + ", and you live in " + userCity);
+alert ("Hello, " + userName + " " + userSurname + ", You are " + userAge + ", keen on " + userHobby + ", and you live in " + userCity);
+
+
+
