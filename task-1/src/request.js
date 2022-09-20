90f1e2943d872972a10d4bd0e7f4d77ed1ab0187
mittnavnermax <104682211+mittnavnermax@users.noreply.github.com>
2022-09-20 20:38:25 +0300

    added request

---
+++ task-1/src/request.js
@@ -0,0 +1,31 @@
+const getData = async () => {
+    try {
+        let response = await fetch("https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11")
+        let currencyData = await response.json();
+        return currencyData;
+        
+    } catch (e) {
+        console.error(e);
+    }
+
+}
+
+const displayData = async () => {
+    try {
+        let result = await getData();
+        
+        let [USD,EUR,ignore,BTC] = result;
+
+        console.log(USD);
+        console.log(EUR);
+        console.log(BTC);
+
+        return [USD,EUR,BTC]
+        
+    } catch (e) {
+        console.error(e);
+    }
+
+}
+
+displayData();
